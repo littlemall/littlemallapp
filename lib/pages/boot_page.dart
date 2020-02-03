31a8c7cd677e0cd800f5049a865b7dec8adf6f42
wurh <ro87630872@126.com>
2020-02-03 17:56:39 +0800

    【UPD】update app 基础架构
    update app 基础架构

---
+++ lib/pages/boot_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BootPage extends StatefulWidget {
+  @override
+  _BootPageState createState() => _BootPageState();
+}
+
+class _BootPageState extends State<BootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Text('boot!!!'),
+      ),
+    );
+  }
+}
---
+++ lib/routers/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import './router_handler.dart';
+import 'package:fluro/fluro.dart';
+
+class Routes{
+  static String root='/';
+  static void configureRoutes(Router router){
+    router.notFoundHandler= new Handler(
+      handlerFunc: (BuildContext context,Map<String,List<String>> params){
+        print('ERROR====>ROUTE WAS NOT FONUND!!!');
+      }
+    );
+  }
+}
