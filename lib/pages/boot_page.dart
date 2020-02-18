a3da4cfda8c5e7cdc35040e726af5ca13f900ee2
wurh <ro87630872@126.com>
2020-02-18 17:40:32 +0800

    【UPD】完成app 登录启动页
    完成app 登录启动页

--- lib/pages/boot_page.dart
+++ lib/pages/boot_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BootPage extends StatefulWidget {
   @override
@@ -6,23 +7,81 @@ class BootPage extends StatefulWidget {
 }
 
 class _BootPageState extends State<BootPage> {
+  bool isLogin = false;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
+    _judgeIsLogin().then((_) {
+      setState(() {
+        isLogin = _;
+      });
+    });
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        title: Text('Home Page'),
-      ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Text('boot!!!'),
-      ),
-    );
+    if (!isLogin) {
+      return Scaffold(
+          body: Container(
+              color: Colors.grey,
+              child: Column(children: [
+                Expanded(
+                  flex: 2,
+                  child: Image.asset(
+                    'assets/images/v1.0/signin_bg@3x.png',
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                      width: double.infinity,
+                      color: Colors.white,
+                      alignment: Alignment.center,
+                      child: Column(children: [
+                        Text(
+                          '欢迎来到小小卖铺',
+                          style: TextStyle(fontSize: 20, color: Colors.grey),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(
+                            top:50,
+                          ),
+                          child: RaisedButton(
+                            onPressed: () {
+                         
+                            },
+                            child: Text('邮箱登录',
+                                style: TextStyle(
+                                    fontSize: 15,
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500)),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)),
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ])),
+                )
+              ])));
+    } else {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: Text('Home Page'),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Text('boot!!!'),
+        ),
+      );
+    }
+  }
+
+  Future<bool> _judgeIsLogin() async {
+    SharedPreferences spf = await SharedPreferences.getInstance();
+    return spf.getString('isLogin') == 'true';
   }
 }
